fc54013dead62ea946d195fd2bbc71c6a6d300fb
Anusha Dharmasena <cdan.dharmasena@gmail.com>
2021-07-08 18:35:29 +1000

    make div and image flush in size

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
 import React from "react";
 
+const IMAGE_WIDTH = 400;
+const IMAGE_HEIGHT = 600;
+
 const App: React.FC = () => {
   return (
     <div style={{ padding: "1em" }}>
@@ -7,8 +10,15 @@ const App: React.FC = () => {
         Trying to see if this image gets precached...
       </p>
 
-      <div style={{ background: "pink", display: "inline-block" }}>
-        <img src="/image.jpg" alt="" width={400} height="auto" />
+      <div
+        style={{ background: "pink", width: IMAGE_WIDTH, height: IMAGE_HEIGHT }}
+      >
+        <img
+          src="/image.jpg"
+          alt=""
+          width={IMAGE_WIDTH}
+          height={IMAGE_HEIGHT}
+        />
       </div>
     </div>
   );
