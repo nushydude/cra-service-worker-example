ead6657e66417524d712d7e0d599d1ac2bb39b4c
Anusha Dharmasena <cdan.dharmasena@gmail.com>
2021-07-08 19:51:50 +1000

    switch to 2nd image

--- src/assetURLs.ts
+++ src/assetURLs.ts
@@ -1,7 +1,7 @@
 // These asset filenames should be MD5ed
 // And this should be the source of truth for asset files
 
-export const IMAGE_MAIN = "/image.jpg";
+export const IMAGE_MAIN = "/image2.jpg";
 export const FONT_MAIN = "/fonts/OpenSans-Regular.ttf";
 
 export const ASSET_URLS = [IMAGE_MAIN, FONT_MAIN];
